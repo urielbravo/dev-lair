7186072a9ce3487a9dc202173d5e2625f75faf26
Uriel Bravo <uriel.bravo.lara@gmail.com>
2022-07-15 02:36:39 -0700

    fetching the posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import Layout from "../../components/layout";
 
 import BlogTitle from "../../components/blogTitle";
@@ -10,4 +12,27 @@ export default function Blog () {
       </div>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map(filename => {
+    const slug = filename.replace('.md', '')
+    
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  console.log('posts:', posts)
+
+  return {
+    props: {}
+  }
 }
